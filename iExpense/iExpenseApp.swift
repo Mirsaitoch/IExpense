8899281aebad80ecbfa73c1ce344cd82ff6760d8
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-11-10 18:45:05 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Мирсаит Сабирзянов on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
