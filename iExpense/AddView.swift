b66c66a0975571175fbab17c7657163943744195
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-11-23 01:05:09 +0300

    add navigationLinks

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    var expenses: Expenses
+
+    @State private var name = "Item"
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    
+    let types = ["Business", "Personal"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Form{
+//                TextField("Name", text: $name)
+                Picker("Type", selection: $type){
+                    ForEach(types, id:\.self){ type in
+                        Text(type)
+                        
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currencySymbol ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .toolbar{
+                Button("Save"){
+                    expenses.items.append(Item(name: name, type: type, amount: amount))
+                    dismiss()
+                }
+            }
+            .navigationTitle($name)
+            .toolbarTitleDisplayMode(.inline)
+        }
+        
     }
 }
 
 #Preview {
-    AddView()
+    AddView(expenses: Expenses())
 }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,93 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+struct Item: Identifiable, Codable{
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+struct productCard: View{
+    
+    var item: Item
+    
+    var body: some View{
+        HStack{
+            VStack(alignment: .leading){
+                Text(item.name).font(.headline)
+                Text(item.type)
+            }
+            Spacer()
+            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                .font(item.amount < 100.0 ? .caption :
+                        (item.amount < 1000.0 && item.amount > 100.0) ? .footnote :
+                        .headline
+            )
         }
-        .padding()
+    }
+}
+
+@Observable
+class Expenses{
+    var items = [Item](){
+        didSet{
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.setValue(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init(){
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decoder = try? JSONDecoder().decode([Item].self, from: savedItems){
+                items = decoder
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+    
+    @State var expenses = Expenses()
+    
+    @State private var showAddPanel = false
+    var body: some View {
+        NavigationStack{
+            List{
+                Section("Personal"){
+                    ForEach(expenses.items){item in
+                        if item.type == "Personal"{
+                            productCard(item: item)
+                        }
+                    }.onDelete(perform: removeItem)
+                }
+                Section("Business"){
+                    ForEach(expenses.items){item in
+                        if item.type == "Business"{
+                            productCard(item: item)
+                        }
+                    }.onDelete(perform: removeItem)
+                }
+            }
+            .toolbar{
+                    NavigationLink{
+                        AddView(expenses: expenses)
+                    } label:{
+                        Image(systemName: "plus")
+                    }
+                        
+                
+            }
+            .navigationTitle("IExpense")
+        }
+    }
+    
+    func removeItem(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
